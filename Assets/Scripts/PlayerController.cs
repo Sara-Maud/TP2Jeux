9652c661ac82e797e463aed841e8b88089523a9f
Janelle <2264502@cegeplimoilou.ca>
2024-09-23 16:06:39 -0400

    Tourner plateforme + joueur

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,15 +4,21 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    private float speed = 0.8f;
+    private Rigidbody playerRb;
+    public GameObject focalPoint;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerRb = GetComponent<Rigidbody>();
+        focalPoint = GameObject.Find("FocalPoint");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float verticalInput = Input.GetAxis("Vertical");
+        playerRb.AddForce(focalPoint.transform.forward * speed * verticalInput);
     }
 }
